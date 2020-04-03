17e73a4919c5712d2e723c6aa2f532e2257449a5
linnefromice <linnefromice@gmail.com>
2020-04-04 02:09:11 +0900

    tutorial - Step 5: Define a resolver

--- graphql-server-example/index.js
+++ graphql-server-example/index.js
@@ -25,4 +25,11 @@ const books = [
         title: 'Jurassic Park',
         author: 'Michael Crichton',
     }
-]
+];
+// Resolvers define the technique for fetching the types defined in the schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+};
