7fade171aadfbe8199c948bf2e769c8eebcd03ca
linnefromice <linnefromice@gmail.com>
2020-04-04 02:04:48 +0900

    tutorial - Step 4: Define your data set

--- graphql-server-example/index.js
+++ graphql-server-example/index.js
@@ -16,3 +16,13 @@ const typeDef = gql`
         books: [Book]
     }
 `;
+const books = [
+    {
+        title: 'Harry Potter and the Chamber of Secrets',
+        author: 'J.K. Rowling',
+    },
+    {
+        title: 'Jurassic Park',
+        author: 'Michael Crichton',
+    }
+]
