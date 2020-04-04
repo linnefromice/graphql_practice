b088b01edee3a6d612126d52a900a4c4f390eaa2
linnefromice <linnefromice@gmail.com>
2020-04-04 16:20:35 +0900

    update example - add schema (author)

--- graphql-server-example/index.js
+++ graphql-server-example/index.js
@@ -8,12 +8,18 @@ const typeDefs = gql`
         title: String
         author: String
     }
+    # EXTRA - Add Schema (Author)
+    type Author {
+        name: String
+        books: [Book]
+    }
 
     # The "Query" type is special: it lists all of the available queries that
     # clients can execute, along with the return type for each. In this
     # case, the "books" query returns an array of zero or more Books (defined above).
     type Query {
         books: [Book]
+        authors: [Author]
     }
 `;
 const books = [
