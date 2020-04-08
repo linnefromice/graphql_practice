4ca7fa2853747d81e103dbe7f79964de387b49e3
linnefromice <linnefromice@gmail.com>
2020-04-08 14:18:19 +0900

    modify - separate models to new file

---
+++ graphql-server-tutorial-one/src/model/index.js
@@ -0,0 +1,45 @@
+
+let users = {
+  1: {
+      id: '1',
+      username: 'Robin Wieruch',
+      messageIds: [1],
+  },
+  2: {
+      id: '2',
+      username: 'Dave David',
+      messageIds: [2],
+  },
+  3: {
+      id: '3',
+      username: 'Mike Ross',
+      messageIds: [],
+  },
+  4: {
+      id: '4',
+      username: 'Harvey Specter',
+      messageIds: [],
+  },
+  5: {
+      id: '5',
+      username: 'Rachel Zane',
+      messageIds: [],
+  }
+};
+let messages = {
+  1: {
+      id: '1',
+      text: 'Hello World',
+      userId: '1',
+  },
+  2: {
+      id: '2',
+      text: 'By World',
+      userId: '2',
+  },
+}
+
+export default {
+  users,
+  messages,
+};
