fce4def29a1233ad96b94201afbf69fad365cd4a
linnefromice <linnefromice@gmail.com>
2020-04-08 14:52:30 +0900

    modify - separate resolvers to new file

---
+++ graphql-server-tutorial-one/src/resolvers/index.js
@@ -0,0 +1,59 @@
+const uuidv4 = require('uuid/v4');
+
+module.exports = {
+  Query: {
+      me: (parent, args, { me }) => {
+          return me;
+      },
+      mike: (parent, args, { mike }) => {
+          return mike;
+      },
+      user: (parent, { id }) => {
+          return model.users[id];
+      },
+      users: () => {
+          return Object.values(model.users);
+      },
+      messages: () => {
+          return Object.values(model.messages);
+      },
+      message: (parent, { id }) => {
+          return model.messages[id];
+      }
+  },
+  Mutation: {
+      createMessage: (parent, { text }, { me }) => {
+          const id = uuidv4();
+          const message = {
+              id,
+              text,
+              userId: me.id,
+          };
+
+          model.messages[id] = message;
+          model.users[me.id].messageIds.push(id);
+
+          return message;
+      },
+      deleteMessage: (parent, { id }) => {
+          const { [id]: message, ...otherMessages } = model.messages;
+          if (!message) {
+              return false;
+          }
+          messages = otherMessages;
+          return true
+      },
+  },
+  User: {
+      messages: user => {
+          return Object.values(model.messages).filter(
+              message => message.userId === user.id,
+          );
+      }
+  },
+  Message: {
+      user: message => {
+          return model.users[message.userId];
+      }
+  }
+};
