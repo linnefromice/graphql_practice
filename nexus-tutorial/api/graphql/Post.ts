5e4728dcf429b0cd709151765146e57202bb5097
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-04-25 20:55:05 +0900

    add Query

--- nexus-tutorial/api/graphql/Post.ts
+++ nexus-tutorial/api/graphql/Post.ts
@@ -1,4 +1,4 @@
-import { objectType } from "nexus"
+import { extendType, objectType } from "nexus"
 
 export const Post = objectType({
   name: 'Post',
@@ -7,5 +7,36 @@ export const Post = objectType({
     t.string('title'),
     t.string('body'),
     t.boolean('published')
+  }
+})
+
+/*
+  t.field('drafts', {
+    type: nonNull(list('Post')),
+  })
+*/
+
+export const PostQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.nonNull.list.field('drafts', {
+      type: 'Post',
+      resolve() {
+        return [
+          {
+            id: 1,
+            title: 'Nexus 1st',
+            body: 'Hello, World!',
+            published: false
+          },
+          {
+            id: 2,
+            title: 'Nexus 2nd',
+            body: 'from GraphQL and Apollo-Server.',
+            published: false
+          },
+        ]
+      }
+    })
   }
 })
