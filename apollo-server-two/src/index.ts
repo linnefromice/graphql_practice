9c735c88fac8b2d60fce87df7acd694d65bb6209
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-04-26 02:12:16 +0900

    enhance api - add comment type & mutation

--- apollo-server-two/src/index.ts
+++ apollo-server-two/src/index.ts
@@ -1,10 +1,36 @@
 import { ApolloServer, gql } from 'apollo-server';
 import fs from 'fs';
 
+interface Comment {
+  id: number;
+  text: string;
+}
+
+// DataSource
+const comments: Comment[] = [
+  {
+    id: 1,
+    text: 'first comment',
+  },
+  {
+    id: 2,
+    text: 'second comment',
+  },
+];
+
+// use GraphQL
 /*
 const typeDefs = gql`
+  type Comment {
+    id: ID!
+    text: String
+  }
   type Query {
     hello: String
+    comments: [Comment!]
+  }
+  type Mutation {
+    addComment(text: String!): Comment!
   }
 `;
 */
@@ -12,6 +38,17 @@ const typeDefs = gql(fs.readFileSync('schema.graphql', 'utf8'));
 const resolvers = {
   Query: {
     hello: () => 'Hello, World! Hello, GraphQL!',
+    comments: () => comments,
+  },
+  Mutation: {
+    addComment: async (parent: any, args: any, context: any) => {
+      const comment = {
+        id: comments.length + 1,
+        text: args.text,
+      };
+      comments.push(comment);
+      return comment;
+    },
   },
 };
 
