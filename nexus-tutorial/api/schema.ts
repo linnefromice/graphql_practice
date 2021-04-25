6c24b9847c739501a53d34454ec11b445e2a308d
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-04-25 20:18:35 +0900

    model a domain named post

--- nexus-tutorial/api/schema.ts
+++ nexus-tutorial/api/schema.ts
@@ -1,10 +1,11 @@
 import { makeSchema } from 'nexus'
 import { join } from 'path'
+import * as types from './graphql'
 
 export const schema = makeSchema({
-    types: [],
-    outputs: {
-        typegen: join(__dirname, '..', 'nexus-typegem.ts'),
-        schema: join(__dirname, '..', 'schema.graphql')
-    }
+  types,
+  outputs: {
+    typegen: join(__dirname, '..', 'nexus-typegem.ts'),
+    schema: join(__dirname, '..', 'schema.graphql')
+  }
 })
