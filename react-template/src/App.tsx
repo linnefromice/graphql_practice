585e2221c237d81223ba62a96224ce8f66f5057c
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-05-12 21:11:01 +0900

    add command for prettier

--- react-template/src/App.tsx
+++ react-template/src/App.tsx
@@ -7,11 +7,7 @@ const App: VFC = () => (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>
-        Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-        and save to reload.
+        Edit <code>src/App.tsx</code> and save to reload.
       </p>
       <a
         className="App-link"
@@ -23,6 +19,6 @@ const App: VFC = () => (
       </a>
     </header>
   </div>
-)
+);
 
 export default App;
