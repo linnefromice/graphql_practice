4f7bd2379ea592734fe7cb52c6233abc320ec69f
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-05-13 10:43:21 +0900

    use graphql api

--- react-gql-client-two/src/App.tsx
+++ react-gql-client-two/src/App.tsx
@@ -1,23 +1,78 @@
 import { VFC } from 'react';
-import logo from './logo.svg';
+import { useQuery, gql } from '@apollo/client';
 import './App.css';
+import { Query, Post } from './graphql/__generated__/graphql';
+
+const GET_DRAFTS = gql`
+  query drafts {
+    drafts {
+      id
+      title
+      body
+      published
+    }
+  }
+`;
+
+const GET_POSTS = gql`
+  query posts {
+    posts {
+      id
+      title
+      body
+      published
+    }
+  }
+`;
+
+const DraftsComponent: VFC = () => {
+  const { loading, error, data } = useQuery<Query>(GET_DRAFTS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.toString()}</p>;
+  if (!data) return <div>Error : data is undefined</div>;
+
+  return (
+    <ul>
+      {data.drafts!.map((draft) => {
+        if (!draft) return <></>;
+        const { id, title, body, published }: Post = draft;
+        return (
+          <li key={`draft.${id!.toString()}`}>
+            {`${title!} / ${body!} / ${published ? 'Published' : 'Draft'}`}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+const PostsComponent: VFC = () => {
+  const { loading, error, data } = useQuery<Query>(GET_POSTS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.toString()}</p>;
+  if (!data) return <div>Error : data is undefined</div>;
+
+  return (
+    <ul>
+      {data.posts!.map((post) => {
+        if (!post) return <></>;
+        const { id, title, body, published }: Post = post;
+        return (
+          <li key={`post.${id!.toString()}`}>
+            {`${title!} / ${body!} / ${published ? 'Published' : 'Draft'}`}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 const App: VFC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+  <div>
+    <h4>Drafts</h4>
+    <DraftsComponent />
+    <h4>Posts</h4>
+    <PostsComponent />
   </div>
 );
 
