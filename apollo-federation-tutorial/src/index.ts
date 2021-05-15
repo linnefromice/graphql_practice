06d78c0291ad4b6e7b6250ef5b0189a8e714faa9
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-05-15 22:50:01 +0900

    add lib - apollo/gateway

--- apollo-federation-tutorial/src/index.ts
+++ apollo-federation-tutorial/src/index.ts
@@ -1,13 +1,18 @@
-import fs from 'fs';
+import { ApolloServer } from 'apollo-server';
+import { ApolloGateway } from '@apollo/gateway';
 
-/*
+const supergraphSchema = '';
+const gateway = new ApolloGateway({
+  supergraphSdl: supergraphSchema,
+});
+
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  gateway,
+  subscriptions: false,
 });
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
-*/
-
-console.log(`🚀  Server ready at URL`);
+server
+  .listen()
+  .then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  })
+  .catch((err) => console.error(err));
